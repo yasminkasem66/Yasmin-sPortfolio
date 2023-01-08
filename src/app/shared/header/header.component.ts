fd707eb0ff9ffd7629e2a83fc8a2ec8b8b5d79e0
yasminkasem55 <yasmin.abdellh@gmail.com>
2023-01-08 12:03:19 +0200

    first commit

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  style,
+  query,
+  transition,
+  stagger,
+  animate,
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('animateMenu', [
+      transition(':enter', [
+        query('*', [
+          style({ opacity: 0, transform: 'translateY(-50%)' }),
+          stagger(50, [
+            animate(
+              '250ms cubic-bezier(0.35, 0, 0.25, 1)',
+              style({ opacity: 1, transform: 'none' })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
